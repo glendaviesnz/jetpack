e424774709a1583c30e78927ae7128d83949e449
Ben Dwyer <ben@scruffian.com>
2020-01-06 14:28:17 +0000

    add a new icon for calendly

--- extensions/blocks/calendly/index.js
+++ extensions/blocks/calendly/index.js
@@ -8,9 +8,9 @@ import { Fragment } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import renderMaterialIcon from '../../shared/render-material-icon';
-import edit from './edit';
 import attributes from './attributes';
+import edit from './edit';
+import icon from './icon';
 
 /**
  * Style dependencies
@@ -30,9 +30,7 @@ export const settings = {
 	),
 
 	/* @TODO Add the icon. You can use one of these https://material.io/tools/icons/?style=outline */
-	icon: renderMaterialIcon(
-		<Path d="M9 15h2V9H9v6zm1-10c-.5 0-1 .5-1 1s.5 1 1 1 1-.5 1-1-.5-1-1-1zm0-4c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z" />
-	),
+	icon,
 
 	category: 'jetpack',
 
